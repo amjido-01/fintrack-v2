6259aeeda8a14c42448c1b3a4538f47fb16cd04b
amjido-01 <youndsadeeq10@gmail.com>
2025-02-06 15:05:55 +0100

    pages

---
+++ app/(routes)/(private)/setting/[workspaceId]/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Page = () => {
+  return (
+    <div>user setting Page</div>
+  )
+}
+
+export default Page
---
+++ app/(routes)/(private)/user/[userId]/workspaces/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Page = () => {
+  return (
+    <div>this is user's workspaces Page</div>
+  )
+}
+
+export default Page
